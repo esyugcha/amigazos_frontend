fdabde0f95313ac4c6fa6b219f2cab1532dc1cb6
Enrique Yugcha <esyugcha@espol.edu.ec>
2024-02-04 13:36:50 -0500

    New Proyect Family (Amigazos)

---
+++ starter-kit/src/pages/index.vue
@@ -0,0 +1,35 @@
+<script setup>
+import ThumbsCarousel from "@/components/ThumbsCarousel.vue";
+</script>
+<template>
+  <div>
+    <VCol
+      cols="12"
+      md="12"
+      class="pa-0"
+    >
+      <ThumbsCarousel />
+    </VCol>
+    <VCard
+      class="mb-6 mt-6"
+      title="Kick start your project 🚀"
+    >
+      <VCardText>All the best for your new project.</VCardText>
+      <VCardText>
+        Please make sure to read our <a
+          href="https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/documentation/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-decoration-none"
+        >
+          Template Documentation
+        </a> to understand where to go from here and how to use our template.
+      </VCardText>
+    </VCard>
+
+    <VCard title="Want to integrate JWT? 🔒">
+      <VCardText>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</VCardText>
+      <VCardText>Please read our  JWT Documentation to get more out of JWT authentication.</VCardText>
+    </VCard>
+  </div>
+</template>
