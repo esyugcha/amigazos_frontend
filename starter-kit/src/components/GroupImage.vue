8a40cb30e1d3249170de1b6754a8fa7b6a564f92
Enrique Yugcha <esyugcha@espol.edu.ec>
2024-02-04 20:18:08 -0500

    Configuration of group image for category of products.

---
+++ starter-kit/src/components/GroupImage.vue
@@ -0,0 +1,81 @@
+<script setup>
+
+import medias from "@images/avatars/medias.jpg";
+import medias2 from "@images/avatars/medias2.jpg";
+import medias3 from "@images/avatars/medias3.jpg";
+const handleClick = () => {
+  // Lógica a ejecutar cuando se hace clic en el avatar
+  console.log("Clic en el avatar");
+};
+</script>
+
+<template>
+  <VRow class="ma-1">
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias2" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias3" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias2" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias2" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias3" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias2" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" sm="6" md="4" lg="2" class="text-center">
+      <VCard @click="handleClick">
+        <VImg :src="medias3" />
+        <VCardText style="font-size: 18px;">Medias</VCardText>
+      </VCard>
+    </VCol>
+  </VRow>
+</template>
